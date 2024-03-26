f8ad1f28364d6194b3408d988357fd03d2ba579d
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-03-26 12:33:38 +0100

    including fields for AN/WI support

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import { eventSource, event_types, getCurrentChatId } from '../../../../script.js';
 import { power_user } from '../../../power-user.js';
 
+let generation_data = {};
+
 eventSource.on(event_types.TEXT_COMPLETION_SETTINGS_READY, (args) =>{
     Object.assign(args, {
         'grimoire': {
             'chat_id': getCurrentChatId(),
-            'instruct': power_user.instruct
+            'instruct': power_user.instruct,
+            'generation_data': generation_data
         }
     });
+});
+
+eventSource.on(event_types.GENERATE_BEFORE_COMBINE_PROMPTS, (args) =>{
+    console.log(args)
+    generation_data = args;
 });
