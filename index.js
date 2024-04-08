8fbf6ecf1d06184d40115bf030168506a7a1cd22
Krakenos <1944193+Krakenos@users.noreply.github.com>
2024-04-08 15:44:10 +0200

    including collapsing newlines setting

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ import { power_user } from '../../../power-user.js';
 let generation_data = {};
 
 eventSource.on(event_types.TEXT_COMPLETION_SETTINGS_READY, (args) =>{
+    let instruct = {
+        'collapse_newlines': power_user.collapse_newlines
+    };
+    Object.assign(instruct, power_user.instruct);
     Object.assign(args, {
         'grimoire': {
             'chat_id': getCurrentChatId(),
-            'instruct': power_user.instruct,
+            'instruct': instruct,
             'generation_data': generation_data
         }
     });
